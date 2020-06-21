7d1237b4211ac9fc8a3510b73471f9ca62020a78
Cierra Johnson-carter <cierrajohnson-carter@Cierras-Air.lan1>
2020-06-21 17:00:31 -0400

    commit

--- contacts/admin.py
+++ contacts/admin.py
@@ -4,7 +4,7 @@ from .models import Contact
 # Register your models here.
 
 
-class ContactAdmin(admin.ModeAdmin):
+class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'listing', 'email', 'contact_date')
     list_display_links = ('id', 'name')
     search_fields = ('name', 'email', 'listing')
