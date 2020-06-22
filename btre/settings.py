9a146f903181de743b76257cb04ce872a9d49ff8
Cierra Johnson-carter <cierrajohnson-carter@Cierras-Air.lan1>
2020-06-21 22:41:36 -0400

    added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -148,3 +148,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'cierradjc@gmail.com'
 EMAIL_HOST_PASSWORD = 'God@love1'
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
